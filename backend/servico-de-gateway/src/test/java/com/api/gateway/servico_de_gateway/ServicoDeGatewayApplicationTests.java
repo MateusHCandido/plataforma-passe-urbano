47af0bacc1a40cb410f9f4e98374fb1ec39744a5
Mateus Henrique <mateushcandido5@gmail.com>
2025-09-13 22:05:04 -0300

    implementação de um api gateway para centralizar requisições

---
+++ backend/servico-de-gateway/src/test/java/com/api/gateway/servico_de_gateway/ServicoDeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gateway.servico_de_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
