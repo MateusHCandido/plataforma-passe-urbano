38047f6ab33e3eef866825eb0967ba6d17885bcb
Mateus Henrique <mateushcandido5@gmail.com>
2025-09-17 17:26:57 -0300

    criação dos services

--- frontend/passe-urbano-front/src/app/services/autenticacao.service.ts
+++ frontend/passe-urbano-front/src/app/services/autenticacao.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators';
+import { Http, Headers, RequestOptions, Response, URLSearchParams } from '@angular/http';
+import { AuditLog } from '../models/logs.model';
+declare var require: any;
+const jwt_decode = require('jwt-decode'); 
 
 @Injectable()
 export class AutenticacaoService {
 
   private authUrl = 'http://localhost:8000/oauth/token'
+  private apiUrl = 'http://localhost:8765/log';
   private clientId = 'urbanape'
   private clientSecret = 'urbanape123'
   
   
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private httpLog: Http
+    ) { }
 
 
   login(email: string, senha: string): Observable<any>{
@@ -28,5 +35,45 @@ export class AutenticacaoService {
       return this.http.post(this.authUrl, body.toString(), { headers });
   }
 
+  buscarLog(): Observable<AuditLog[]>{
+    const emailUsuario = this.getEmail();
+    const permissaoUsuario = this.getPermissao();
+    
+    const token = localStorage.getItem('access_token');
+
+    const headers = new Headers({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    const params: URLSearchParams = new URLSearchParams();
+    params.set('emailUsuario', emailUsuario);
+    params.set('tipoUsuario', permissaoUsuario)
+
+    const options = new RequestOptions({ headers: headers, search: params });
+
+    return this.httpLog
+      .get(`${this.apiUrl}/logs`, options)
+      .map((res: Response) => res.json() as AuditLog[]);
+  }
+
+
+  getPermissao(): string{
+    const decodeToken = jwt_decode(localStorage.getItem('access_token'));
+    return decodeToken.authorities[0];
+  }
+
+  getEmail(): string{
+    const decodeToken = jwt_decode(localStorage.getItem('access_token'));
+    const emailUsuarioLogado = decodeToken.user_name;
+    return emailUsuarioLogado;
+  }
+
+  isAdmin(): boolean{
+    const role = this.getPermissao();
+    return role === 'ROLE_ADMIN';
+  }
+
+  
   
 }
