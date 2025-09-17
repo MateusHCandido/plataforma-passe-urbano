3642c54b55b1ec4ecb0e4448efe40757aeec073c
Mateus Henrique <mateushcandido5@gmail.com>
2025-09-17 17:21:20 -0300

    Ajustes no serviço de gateway

--- backend/servico-de-gateway/src/main/java/com/api/gateway/servico_de_gateway/config/CorsConfig.java
+++ backend/servico-de-gateway/src/main/java/com/api/gateway/servico_de_gateway/config/CorsConfig.java
@@ -1,36 +1,28 @@
-package com.eureka.servico_descoberta.config;
+package com.api.gateway.servico_de_gateway.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean(){
-        List<String> all = Collections.singletonList("*");
-
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); // Angular
-        corsConfiguration.setAllowedHeaders(all);
-        corsConfiguration.setAllowedMethods(all);
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfig.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
+        source.registerCorsConfiguration("/**", corsConfig);
 
-        CorsFilter corsFilter = new CorsFilter(source);
-        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
-        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return filter;
+        return new CorsWebFilter(source);
     }
 }
