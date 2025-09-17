01d3aef8444d21ae46fb9620c8642c5f08099ef2
Mateus Henrique <mateushcandido5@gmail.com>
2025-09-17 17:24:22 -0300

    atualização no script inicial da aplicação para se adaptar a regra de apresentação do front e eliminar problemas de constraints no backend

--- backend/sql-init/init.sql
+++ backend/sql-init/init.sql
@@ -9,6 +9,20 @@ CREATE TABLE IF NOT EXISTS service.usuario_cartao (
     senha VARCHAR(200) NOT NULL
 );
 
+-- Inserindo usuários no service.usuario_cartao
+INSERT INTO service.usuario_cartao (nome, email, senha)
+VALUES (
+ 'Mateus',
+ 'admin@sistema.com',
+ '$2a$10$Fu5MvHneyEJdBYi.rmQpjOimSA/CcN351O03PnQf.LCNKTL/Lr03K'-- senha já criptografada
+);
+
+  -- Sincronizar a sequence do BIGSERIAL
+SELECT setval(
+  'service.usuario_cartao_usuario_id_seq',
+  (SELECT COALESCE(MAX(usuario_id), 0) FROM service.usuario_cartao)
+);
+
 --Criação da tabela de cartões
 CREATE TABLE IF NOT EXISTS service.cartao (
     cartao_id BIGSERIAL PRIMARY KEY,
@@ -20,14 +34,6 @@ CREATE TABLE IF NOT EXISTS service.cartao (
     CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES service.usuario_cartao(usuario_id)
 );
 
---inicia com usuário no service.usuario_cartao
- INSERT INTO service.usuario_cartao (nome, email, senha)
- VALUES (
-     'Mateus',
-     'admin@sistema.com',
-     '$2a$10$Fu5MvHneyEJdBYi.rmQpjOimSA/CcN351O03PnQf.LCNKTL/Lr03K' -- senha já criptografada
-);
-
 -- Criar sequence para numero_cartao
 CREATE SEQUENCE IF NOT EXISTS service.seq_cartao_numero START 1000000000 INCREMENT 1;
 
@@ -35,10 +41,27 @@ CREATE SEQUENCE IF NOT EXISTS service.seq_cartao_numero START 1000000000 INCREME
 ALTER TABLE service.cartao
     ALTER COLUMN numero_cartao SET DEFAULT nextval('service.seq_cartao_numero');
 
+-- Ssincroniza a sequence do cartao_id
+SELECT setval(
+  'service.cartao_cartao_id_seq',
+  COALESCE((SELECT MAX(cartao_id) FROM service.cartao), 1)
+);
+
 -- Criação da tabela de permissões
 CREATE TABLE auth.roles (
     id BIGINT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE service.audit_log (
+    id BIGSERIAL PRIMARY KEY,
+    usuario_email VARCHAR(255) NOT NULL,
+    acao VARCHAR(100) NOT NULL,
+    entidade VARCHAR(50) NOT NULL,
+    entidade_id BIGINT,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    detalhe TEXT
 );
 
 -- Criação da tabela de usuários com relação 1:N para roles
@@ -58,14 +81,14 @@ INSERT INTO auth.roles (id, nome) VALUES
     (2, 'USER');
 
 --inicia com usuário com role admin
-INSERT INTO auth.usuarios (nome, email, senha, role_id, data_criacao)
-VALUES (
-    'Mateus',
-    'admin@sistema.com',
-    '$2a$10$Fu5MvHneyEJdBYi.rmQpjOimSA/CcN351O03PnQf.LCNKTL/Lr03K', -- senha já criptografada
-    1,
-    CURRENT_TIMESTAMP
-  );
+INSERT INTO auth.usuarios (nome, email, senha, role_id, data_criacao) VALUES
+('Mateus Henrique ADM', 'admin@sistema.com','$2a$10$Fu5MvHneyEJdBYi.rmQpjOimSA/CcN351O03PnQf.LCNKTL/Lr03K', 1, CURRENT_TIMESTAMP),
+('Mateus Henrique', 'mateus@example.com', '$2a$10$PZqzEug7PUuAp5zvHoLLSu9SzFpI6jYMpM.Nr83J5re5thPYFgV9u', 2, CURRENT_TIMESTAMP),
+('João Silva', 'joao@example.com', '$2a$10$PZqzEug7PUuAp5zvHoLLSu9SzFpI6jYMpM.Nr83J5re5thPYFgV9u', 2, CURRENT_TIMESTAMP),
+('Maria Oliveira', 'maria@example.com', '$2a$10$PZqzEug7PUuAp5zvHoLLSu9SzFpI6jYMpM.Nr83J5re5thPYFgV9u', 2, CURRENT_TIMESTAMP),
+('Ana Souza', 'ana@example.com', '$2a$10$PZqzEug7PUuAp5zvHoLLSu9SzFpI6jYMpM.Nr83J5re5thPYFgV9u', 2, CURRENT_TIMESTAMP),
+('Pedro Costa', 'pedro@example.com', '$2a$10$PZqzEug7PUuAp5zvHoLLSu9SzFpI6jYMpM.Nr83J5re5thPYFgV9u', 2, CURRENT_TIMESTAMP);
+
 
 
 
